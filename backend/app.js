f7c794fae027761c7eee56154f78921743a322f6
sahanareddy18 <sahanareddykanukuntla18@gmail.com>
2026-04-29 01:48:17 +0530

    your changes

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,9 @@ import expensesRouter from './routes/expenses.js';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 app.get('/', (req, res) => {
